c9d3eacd851d6a289bf56981dfb743c9c51f5b22
Yifan Lu <me@yifanlu.com>
2012-05-28 20:04:22 -0500

    Initial Commit

---
+++ iMXUSBTool/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  iMXUSBTool
+//
+//  Created by Yifan Lu on 5/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
