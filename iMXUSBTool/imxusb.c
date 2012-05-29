f78efb8220cd86b29b7a4e34426c19190e752c4e
Yifan Lu <me@yifanlu.com>
2012-05-28 20:52:41 -0500

    get device function

--- iMXUSBTool/imxusb.c
+++ iMXUSBTool/imxusb.c
@@ -8,3 +8,23 @@
 
 #include <stdio.h>
 #include "imxusb.h"
+#include "hidapi.h"
+#include <unistd.h>
+
+// this function will block until it finds a device
+hid_device *imx50_get_device() {
+    hid_device *handle = NULL;
+    struct hid_device_info *dev;
+    
+    while(handle == NULL) {
+        dev = hid_enumerate(IMX50_VID, IMX50_PID);
+        if(dev == NULL) {
+            sleep(5);
+            continue; // loop
+        }
+        handle = hid_open(dev->vendor_id, dev->product_id, dev->serial_number);
+        hid_free_enumeration(dev);
+    }
+    
+    return handle;
+}
